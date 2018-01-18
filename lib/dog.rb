a003ad5a4107f7c0eced2b14a4233c5944146220
Guilherme Reis <gui.bertoni@gmail.com>
2018-01-18 00:32:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,15 +15,4 @@ class Dog
 
   fido.name="Snoopy"
 
-  def initialize(breed)
-    @breed=breed
-  end
-
-  def breed
-    @breed
-  end
-  def breed=(new_breed)
-    @breed=new_breed
-  end
-
 end
